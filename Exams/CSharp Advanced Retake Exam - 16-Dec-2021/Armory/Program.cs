89b9d70c1be76e414d9d5f46a94fa243dd089576
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-04 23:04:57 +0300

    Retake Exam

---
+++ Exams/CSharp Advanced Retake Exam - 16-Dec-2021/Armory/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Linq;
+
+namespace Armory
+{
+    class Program
+    {
+        private static int officerRow;
+        private static int officerCol;
+        private static char[,] matrix;
+        private static int gold;
+        private static string direction;
+        private static int firstMirrorRow = -1;
+        private static int firstMirrorCol ;
+        private static int secondMirrorRow ;
+        private static int secondMirrorCol ;
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            matrix = new char[matrixSize, matrixSize];
+           
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string currentRow = Console.ReadLine();
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                    if (matrix[row, col] == 'A')
+                    {
+                        officerRow = row;
+                        officerCol = col;
+                    }
+                    else if (matrix[row, col] == 'M')
+                    {
+                        if (firstMirrorRow == -1)
+                        {
+                            firstMirrorRow = row;
+                            firstMirrorCol = col;
+                        }
+                        else
+                        {
+                            secondMirrorRow = row;
+                            secondMirrorCol = col;
+                        }
+                    }
+                }
+            }
+            string command = Console.ReadLine();
+            bool outOfArmory = false;
+            while (true)
+            {
+                direction = command;
+                if (command == "up")
+                {
+                    if (!OutOfArmory(officerRow - 1, officerCol))
+                    {
+                        outOfArmory = true;
+                        matrix[officerRow, officerCol] = '-';
+                        break;
+                    }
+                    Move(-1, 0);
+                }
+                else if (command == "down")
+                {
+                    if (!OutOfArmory(officerRow + 1, officerCol))
+                    {
+                        outOfArmory = true;
+                        matrix[officerRow, officerCol] = '-';
+                        break;
+                    }
+                    Move(1, 0);
+                }
+                else if (command == "left")
+                {
+                    if (!OutOfArmory(officerRow, officerCol - 1))
+                    {
+                        outOfArmory = true;
+                        matrix[officerRow, officerCol] = '-';
+                        break;
+                    }
+                    Move(0, -1);
+                }
+                else if (command == "right")
+                {
+                    if (!OutOfArmory(officerRow, officerCol + 1))
+                    {
+                        outOfArmory = true;
+                        matrix[officerRow, officerCol] = '-';
+                        break;
+                    }
+                    Move(0, 1);
+                }
+                if (gold >= 65)
+                {
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+            if (outOfArmory)
+            {
+                Console.WriteLine($"I do not need more swords!");
+            }
+            else
+            {
+                Console.WriteLine($"Very nice swords, I will come back for more!");
+            }
+            Console.WriteLine($"The king paid {gold} gold coins. ");
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row,col]}");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Move(int row, int col)
+        {
+            if (direction == "up")
+            {
+                if (matrix[officerRow - 1, officerCol] == 'M')
+                {
+                    MirrorFound(officerRow - 1, officerCol);
+                }
+               else  if (char.IsDigit(matrix[officerRow-1, officerCol]))
+                {
+                    gold += (int)Char.GetNumericValue((matrix[officerRow - 1, officerCol]));
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow-1, officerCol] = 'A';
+                    officerRow = officerRow - 1;
+                }
+                else
+                {
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow - 1, officerCol] = 'A';
+                    officerRow = officerRow + 1;
+                }
+            }
+            else if (direction == "down")
+            {
+                if (matrix[officerRow +1, officerCol] == 'M')
+                {
+                    MirrorFound(officerRow + 1, officerCol);
+                }
+                else if (char.IsDigit(matrix[officerRow + 1, officerCol]))
+                {
+                    gold += (int)Char.GetNumericValue((matrix[officerRow + 1, officerCol]));
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow + 1, officerCol] = 'A';
+                    officerRow = officerRow + 1;
+                }
+                else
+                {
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow + 1, officerCol] = 'A';
+                    officerRow = officerRow + 1;
+                }
+            }
+            else if (direction == "left")
+            {
+                if (matrix[officerRow, officerCol-1] == 'M')
+                {
+                    MirrorFound(officerRow, officerCol-1);
+                }
+                else if (char.IsDigit(matrix[officerRow, officerCol-1]))
+                {
+                    gold += (int)Char.GetNumericValue((matrix[officerRow, officerCol - 1]));
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow, officerCol-1] = 'A';
+                    officerCol = officerCol - 1;
+                }
+                else
+                {
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow, officerCol-1] = 'A';
+                    officerCol = officerCol - 1;
+                }
+            }
+            else if (direction == "right")
+            {
+                if (matrix[officerRow, officerCol+1] == 'M')
+                {
+                    MirrorFound(officerRow, officerCol+1);
+                }
+               else  if (char.IsDigit(matrix[officerRow, officerCol + 1]))
+                {
+                    gold += (int)Char.GetNumericValue((matrix[officerRow, officerCol + 1]));
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow, officerCol + 1] = 'A';
+                    officerCol = officerCol + 1;
+                }
+                else
+                {
+                    matrix[officerRow, officerCol] = '-';
+                    matrix[officerRow, officerCol + 1] = 'A';
+                    officerCol = officerCol + 1;
+                }
+            }
+     
+        }
+        public static bool OutOfArmory(int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+        public static void MirrorFound(int mirRow , int mirCol)
+        {
+            matrix[officerRow, officerCol] = '-';
+           if (firstMirrorRow == mirRow && firstMirrorCol == mirCol)
+            {
+                officerRow = secondMirrorRow;
+                officerCol = secondMirrorCol;
+                matrix[mirRow, mirCol] = '-';
+                matrix[officerRow, officerCol] = 'A';
+            }
+           else if (secondMirrorRow == mirRow && secondMirrorCol == mirCol)
+            {
+                officerRow = firstMirrorRow;
+                officerCol = firstMirrorCol;
+                matrix[mirRow, mirCol] = '-';
+                matrix[officerRow, officerCol] = 'A';
+            }
+        }
+
+    }
+}
---
+++ Exams/CSharp Advanced Retake Exam - 16-Dec-2021/Blacksmith/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_Advanced_Retake_Exam___16_Dec_2021
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Queue<int> steels = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> carbon = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Dictionary<string, int> forgedSwords = new Dictionary<string, int>();
+            while (steels.Count > 0 && carbon.Count > 0)
+            {
+                int currentCarbon = carbon.Peek();
+                int sum = steels.Peek() + currentCarbon;
+                if (sum == 70)
+                {
+                    RemoveMaterial(steels, carbon);
+                    ForgedSwords(forgedSwords, "Gladius");
+                }
+                else if (sum == 80)
+                {
+                    RemoveMaterial(steels, carbon);
+                    ForgedSwords(forgedSwords, "Shamshir");
+                }
+                else if (sum == 90)
+                {
+                    RemoveMaterial(steels, carbon);
+                    ForgedSwords(forgedSwords, "Katana");
+                }
+                else if (sum == 110)
+                {
+                    RemoveMaterial(steels, carbon);
+                    ForgedSwords(forgedSwords, "Sabre");
+                }
+                else if (sum == 150)
+                {
+                    RemoveMaterial(steels, carbon);
+                    ForgedSwords(forgedSwords, "Broadsword");
+                }
+                else
+                {
+                    RemoveMaterial(steels, carbon);
+                    currentCarbon += 5;
+                    carbon.Push(currentCarbon);
+
+                }
+            }
+            if (forgedSwords.Values.Sum() > 0)
+            {
+                Console.WriteLine($"You have forged {forgedSwords.Values.Sum()} swords.");
+            }
+            else
+            {
+                Console.WriteLine($"You did not have enough resources to forge a sword.");
+            }
+            if (steels.Count == 0)
+            {
+                Console.WriteLine($"Steel left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Steel left: {string.Join(", ",steels)}");
+            }
+            if (carbon.Count == 0)
+            {
+                Console.WriteLine($"Carbon left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Carbon left: {string.Join(", ", carbon)}");
+            }
+            foreach (var item in forgedSwords.OrderBy(s=>s.Key))
+            {
+                if (item.Value > 0)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+        
+        public static void RemoveMaterial(Queue<int> steels, Stack<int> carbon)
+        {
+            steels.Dequeue();
+            carbon.Pop();
+        }
+        public static void ForgedSwords(Dictionary<string, int> listOfSwords,string sword)
+        {
+            if (!listOfSwords.ContainsKey(sword))
+            {
+                listOfSwords.Add(sword, 1);
+            }
+            else
+            {
+                listOfSwords[sword] += 1;
+            }
+        }
+    }
+}
