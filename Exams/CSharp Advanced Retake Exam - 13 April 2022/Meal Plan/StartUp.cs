7d915f47be96059a1748423f4c1a6cfe5e7784d7
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-04 23:12:36 +0300

    Retake Exam

---
+++ Exams/CSharp Advanced Retake Exam - 13 April 2022/Meal Plan/StartUp.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MealPlan
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> meals = new Queue<string>(Console.ReadLine().Split());
+            Stack<int> caloriesPerDay = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+           int mealsCount = meals.Count;
+            int mealCalories = 0;
+            int dailyCalories = 0;
+            string currenMeal = "";
+            while (meals.Count > 0 && caloriesPerDay.Count > 0)
+            {
+
+                if (dailyCalories == 0)
+                {
+                    dailyCalories = caloriesPerDay.Peek();
+                }
+                if (mealCalories == 0)
+                {
+                    currenMeal = meals.Peek();
+                }
+                else
+                {
+                    if (dailyCalories - mealCalories >= 0)
+                    {
+                        dailyCalories -= mealCalories;
+                        mealCalories = 0;
+                        meals.Dequeue();
+                    }
+                    else
+                    {
+                        mealCalories -= dailyCalories;
+                        dailyCalories = 0;
+                        caloriesPerDay.Pop();
+                        if (caloriesPerDay.Count == 0)
+                        {
+                            meals.Dequeue();
+                        }
+                    }
+                    continue;
+                }
+                if (currenMeal == "salad")
+                {
+                    mealCalories = 350;
+                    if (dailyCalories - mealCalories >= 0)
+                    {
+                        dailyCalories -= mealCalories;
+                        mealCalories = 0;
+                        meals.Dequeue();
+                    }
+                    else
+                    {
+                        mealCalories -= dailyCalories;
+                        dailyCalories = 0;
+                        caloriesPerDay.Pop();
+                        if (caloriesPerDay.Count == 0)
+                        {
+                            meals.Dequeue();
+                        }
+                    }
+                }
+                else if (currenMeal == "soup")
+                {
+                    mealCalories = 490;
+                    if (dailyCalories - mealCalories >= 0)
+                    {
+                        dailyCalories -= mealCalories;
+                        mealCalories = 0;
+                        meals.Dequeue();
+                    }
+                    else
+                    {
+                        mealCalories -= dailyCalories;
+                        dailyCalories = 0;
+                        caloriesPerDay.Pop();
+                        if (caloriesPerDay.Count == 0)
+                        {
+                            meals.Dequeue();
+                        }
+                    }
+                }
+                else if (currenMeal == "pasta")
+                {
+                    mealCalories = 680;
+                    if (dailyCalories - mealCalories >= 0)
+                    {
+                        dailyCalories -= mealCalories;
+                        mealCalories = 0;
+                        meals.Dequeue();
+                    }
+                    else
+                    {
+                        mealCalories -= dailyCalories;
+                        dailyCalories = 0;
+                        caloriesPerDay.Pop();
+                        if (caloriesPerDay.Count == 0)
+                        {
+                            meals.Dequeue();
+                        }
+                    }
+                }
+                else if (currenMeal == "steak")
+                {
+                    mealCalories = 790;
+                    if (dailyCalories - mealCalories >= 0)
+                    {
+                        dailyCalories -= mealCalories;
+                        mealCalories = 0;
+                        meals.Dequeue();
+                    }
+                    else
+                    {
+                        mealCalories -= dailyCalories;
+                        dailyCalories = 0;
+                        caloriesPerDay.Pop();
+                        if (caloriesPerDay.Count == 0)
+                        {
+                            meals.Dequeue();
+                        }
+                    }
+                }
+            }
+            if (meals.Count == 0)
+            {
+                caloriesPerDay.Pop();
+                caloriesPerDay.Push(dailyCalories);
+                Console.WriteLine($"John had {mealsCount} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", caloriesPerDay)} calories.");
+            }
+            else
+            {
+                Console.WriteLine($"John ate enough, he had {mealsCount -meals.Count} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+            }
+        }
+    }
+}
---
+++ Exams/CSharp Advanced Retake Exam - 13 April 2022/Truffle Hunter/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace Truffle_Hunter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int matrixSize = int.Parse(Console.ReadLine());
+            string[,] matrix = new string[matrixSize, matrixSize];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                string[] currentRow = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+            string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int peterBlackTrufflesCount = 0;
+            int peterSummerTrufflesCount = 0;
+            int peterWhiteTrufflesCount = 0;
+            int boardEatenTrufflesCount = 0;
+            while (command[0] != "Stop" && command[1] != "the" && command[2] != "hunt")
+            {
+                int row = int.Parse(command[1]);
+                int col = int.Parse(command[2]);
+                string direction = "";
+                if (command.Length == 4)
+                {
+                    direction = command[3];
+                }
+                if (command[0] == "Collect")
+                {
+                    if (matrix[row, col] == "B")
+                    {
+                        peterBlackTrufflesCount += 1;
+                        matrix[row, col] = "-";
+                    }
+                    else if (matrix[row, col] == "S")
+                    {
+                        peterSummerTrufflesCount += 1;
+                        matrix[row, col] = "-";
+                    }
+                    else if (matrix[row, col] == "W")
+                    {
+                        peterWhiteTrufflesCount += 1;
+                        matrix[row, col] = "-";
+                    }
+                }
+                else
+                {
+                    if (direction == "up")
+                    {
+                        for (int i = row; i >= 0; i -= 2)
+                        {
+                            if (matrix[i, col] == "B")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[i, col] = "-";
+                            }
+                            else if (matrix[i, col] == "S")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[i, col] = "-";
+                            }
+                            else if (matrix[i, col] == "W")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[i, col] = "-";
+                            }
+                        }
+                    }
+                    else if (direction == "down")
+                    {
+                        for (int i = row; i < matrix.GetLength(0); i += 2)
+                        {
+                            if (matrix[i, col] == "B")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[i, col] = "-";
+                            }
+                            else if (matrix[i, col] == "S")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[i, col] = "-";
+                            }
+                            else if (matrix[i, col] == "W")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[i, col] = "-";
+                            }
+                        }
+                    }
+                    else if (direction == "left")
+                    {
+                        for (int i = col; i >= 0; i-=2)
+                        {
+                            if (matrix[row, i] == "B")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[row, i] = "-";
+                            }
+                            else if (matrix[row, i] == "S")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[row, i] = "-";
+                            }
+                            else if (matrix[row, i] == "W")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[row, i] = "-";
+                            }
+                        }
+                    }
+                    else if (direction == "right")
+                    {
+                        for (int i = col; i < matrix.GetLength(1); i += 2)
+                        {
+                            if (matrix[row, i] == "B")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[row, i] = "-";
+                            }
+                            else if (matrix[row, i] == "S")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[row, i] = "-";
+                            }
+                            else if (matrix[row, i] == "W")
+                            {
+                                boardEatenTrufflesCount += 1;
+                                matrix[row, i] = "-";
+                            }
+                        }
+
+                    }
+                }
+                command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            Console.WriteLine($"Peter manages to harvest {peterBlackTrufflesCount} black, {peterSummerTrufflesCount} summer, and {peterWhiteTrufflesCount} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {boardEatenTrufflesCount} truffles.");
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row,col]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
