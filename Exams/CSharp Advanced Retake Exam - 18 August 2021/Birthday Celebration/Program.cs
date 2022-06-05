fbf73f557fb207d0b05b2449326b1b1186b0d5c4
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-05 22:43:12 +0300

    Retake Exam

---
+++ Exams/CSharp Advanced Retake Exam - 18 August 2021/Birthday Celebration/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp_Advanced_Retake_Exam___18_August_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> guest = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> plates = new Stack<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int wasteFood = 0;
+            int currentGuest = 0;
+            while (guest.Count > 0 && plates.Count > 0)
+            {
+                if (currentGuest == 0)
+                {
+                currentGuest = guest.Peek();
+                }
+                int currentPlate = plates.Peek();
+                if (currentGuest > currentPlate)
+                {
+                    currentGuest -= currentPlate;
+                    currentPlate = 0;
+                    plates.Pop();
+                }
+                else if (currentGuest == currentPlate)
+                {
+                    guest.Dequeue();
+                    plates.Pop();
+                    currentGuest = 0;
+                    currentPlate = 0;
+                }
+                else if (currentGuest < currentPlate)
+                {
+                    wasteFood += currentPlate - currentGuest;
+                    currentGuest = 0;
+                    guest.Dequeue();
+                    plates.Pop();
+                }
+            }
+            if (guest.Count > 0)
+            {
+                Console.WriteLine($"Guests: {string.Join(" ",guest)}");
+            }
+            else if (plates.Count > 0)
+            {
+                Console.WriteLine($"Plates: {string.Join(" ", plates)}");
+            }
+            Console.WriteLine($"Wasted grams of food: {wasteFood}");
+        }
+    }
+}
---
+++ Exams/CSharp Advanced Retake Exam - 18 August 2021/StreetRacing/Race.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        private List<Car> participants;
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            participants = new List<Car>();
+        }
+
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+        public List<Car> Participants => participants;
+        public int Count => Participants.Count;
+        public void Add(Car car)
+        {
+            string license = participants.Any(s => s.LicensePlate == car.LicensePlate).ToString();
+            if (license == "False" && participants.Count + 1 <= Capacity && car.HorsePower <= MaxHorsePower)
+            {
+                participants.Add(car);
+            }
+        }
+        public bool Remove(string licensePlate)
+        {
+            Car removeCar = participants.FirstOrDefault(s => s.LicensePlate == licensePlate);
+            if (removeCar != null)
+            {
+                participants.Remove(removeCar);
+                return true;
+            }
+            return false;
+        }
+        public Car FindParticipant(string licensePlate)
+        {
+            Car findCar = participants.FirstOrDefault(s => s.LicensePlate == licensePlate);
+            if (findCar != null)
+            {
+                return findCar;
+            }
+            return null;
+        }
+        public Car GetMostPowerfulCar()
+        {
+            if (participants.Count == 0)
+            {
+                return null;
+            }
+            else
+            {
+                Car mostPowerful = participants.OrderByDescending(s => s.HorsePower).First();
+                return mostPowerful;
+            }
+        }
+        public string Report()
+        {
+            StringBuilder report = new StringBuilder();
+            report.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            foreach (var car in participants)
+            {
+                report.AppendLine($"Make: {car.Make}");
+                report.AppendLine($"Model: {car.Model}");
+                report.AppendLine($"License Plate: {car.LicensePlate}");
+                report.AppendLine($"Horse Power: {car.HorsePower}");
+                report.AppendLine($"Weight: {car.Weight}");
+            }
+            return report.ToString().TrimEnd();
+        }
+    }
+}
