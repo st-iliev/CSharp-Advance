437bd14ccbdbb11699f9d417bcda55bdd78dbe61
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-12 20:48:28 +0300

    Add Exercise solution

---
+++ Generic - Exercises/03. Generic Swap Method String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Exercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lineOfText = int.Parse(Console.ReadLine());
+            List<string> list = new List<string>();
+            for (int i = 0; i < lineOfText; i++)
+            {
+                string line =Console.ReadLine();
+                list.Add(line);
+            }
+                var box = new Box<string>(list);
+            int[] indexes = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            box.Swap(list, indexes[0], indexes[1]);
+            Console.WriteLine(box);
+
+        }
+    }
+}
