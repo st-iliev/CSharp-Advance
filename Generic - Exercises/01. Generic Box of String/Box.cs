6c098c534b33078efd2d181da1848a6aa476b658
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-12 20:46:09 +0300

    Exercises

---
+++ Generic - Exercises/01. Generic Box of String/Box.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Exercise
+{
+    public class Box<T>
+    {
+        public T Element { get; }
+        public Box(T element)
+        {
+            Element = element;
+        }
+        public override string ToString()
+        {
+            return $"{typeof(T)}: {Element}";
+        }
+    }
+}
---
+++ Generic - Exercises/01. Generic Box of String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Generic_Exercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lineOfText = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lineOfText; i++)
+            {
+                string line = Console.ReadLine();
+                var box = new Box<string>(line);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
