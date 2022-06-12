654b83cad3bf8256e203a13e389b7baa69f53191
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-12 20:52:12 +0300

    Add Exercise Solution

---
+++ Generic - Exercises/08. Threeuple/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Exercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string personName = $"{personInfo[0]} {personInfo[1]}";
+            string city = personInfo[3];
+            if (personInfo.Length > 4)
+            {
+            city = $"{personInfo[3]} {personInfo[4]}";
+            }
+            Threeuple<string, string,string> firstPerson = new Threeuple<string, string,string>(personName,personInfo[2], city);
+
+            string[] name = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var isDrunk = name[2] == "drunk" ? true : false;
+            Threeuple<string, int,bool> secondPerson = new Threeuple<string, int,bool>(name[0], int.Parse(name[1]), isDrunk);
+
+            string[] bankAccountInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Threeuple<string, double,string> thirdPerson = new Threeuple<string, double,string>(bankAccountInfo[0], double.Parse(bankAccountInfo[1]), bankAccountInfo[2]);
+
+            Console.WriteLine(firstPerson);
+            Console.WriteLine(secondPerson);
+            Console.WriteLine(thirdPerson);
+        }
+    }
+}
---
+++ Generic - Exercises/08. Threeuple/Threeuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Exercise
+{
+    public class Threeuple<T1,T2,T3>
+    {
+        public Threeuple(T1 first , T2 second , T3 third)
+        {
+            First = first;
+            Second = second;
+            Third = third;
+        }
+        public T1 First { get; set; }
+        public T2 Second { get; set; }
+        public T3 Third { get; set; }
+        public override string ToString()
+        {
+            return $"{First} -> {Second} -> {Third}";
+        }
+    }
+}
