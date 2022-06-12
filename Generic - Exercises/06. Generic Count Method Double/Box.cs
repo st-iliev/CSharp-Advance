88c99e037619b2793cd708c894e2616e5b311934
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-12 20:50:48 +0300

    Add Exercise Solution

---
+++ Generic - Exercises/06. Generic Count Method Double/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generic_Exercise
+{
+    public class Box<T>: IComparable<T> where T : IComparable<T>
+    {
+        public T Element { get; }
+        public Box(T element)
+        {
+            Element = element;
+        }
+
+        public Box(List<T> elements)
+        {
+            Elements = elements;
+        }
+        public void Swap(List<T> elements,int firstIndex , int secondIndex)
+        {
+            T firstElement = elements[firstIndex];
+            elements[firstIndex] = elements[secondIndex];
+            elements[secondIndex] = firstElement;
+        }
+        public List<T> Elements { get;}
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in Elements)
+            {
+                sb.AppendLine($"{item.GetType()}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        public int CompareTo(T other)
+          => Element.CompareTo(other);
+
+        public int CountOfElemenets<T>(List<T> list, T elemenet) where T : IComparable
+        {
+            return list.Count(s => s.CompareTo(elemenet) > 0);
+        }
+    }
+}
---
+++ Generic - Exercises/06. Generic Count Method Double/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Exercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int elements = int.Parse(Console.ReadLine());
+            List<double> list = new List<double>();
+            for (int i = 0; i < elements; i++)
+            {
+                double currentElement =double.Parse(Console.ReadLine());
+                list.Add(currentElement);
+            }
+            double elementCompare = double.Parse(Console.ReadLine());
+            var box = new Box<double>(list);
+            int count = box.CountOfElemenets(list, elementCompare);
+            Console.WriteLine(count);
+
+        }
+    }
+}
