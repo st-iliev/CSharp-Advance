fb3b425a3b7a15743c344ac382aaf4103960b198
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-12 20:51:27 +0300

    Add Exercise Solution

---
+++ Generic - Exercises/07. Tuple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Generic_Exercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] personInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string personName = $"{personInfo[0]} {personInfo[1]}";
+            Tupe<string, string> firstPerson = new Tupe<string, string>(personName,personInfo[2]);
+            string[] name = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Tupe<string, int> secondPerson = new Tupe<string, int>(name[0], int.Parse(name[1]));
+            double[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            Tupe<int, double> thirdPerson = new Tupe<int, double>((int)numbers[0], numbers[1]);
+            Console.WriteLine(firstPerson);
+            Console.WriteLine(secondPerson);
+            Console.WriteLine(thirdPerson);
+        }
+    }
+}
---
+++ Generic - Exercises/07. Tuple/Tupe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Exercise
+{
+    public class Tupe<T1,T2>
+    {
+        public Tupe(T1 first ,T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+        public T1 First { get; set; }
+        public T2 Second { get; set; }
+        public override string ToString()
+        {
+            return $"{First} -> {Second}";
+        }
+    }
+}
