a2497015455c73b42f135b7a81e55391bd67dc7f
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-12 20:47:31 +0300

    Add Exercise Solution

---
+++ Generic - Exercises/02. Generic Box of Integer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GenericExercise
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lineOfText = int.Parse(Console.ReadLine());
+            for (int i = 0; i < lineOfText; i++)
+            {
+                int line = int.Parse(Console.ReadLine());
+                var box = new Box<int>(line);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
