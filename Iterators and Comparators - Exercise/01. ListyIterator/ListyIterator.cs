5a9d1bb5c8b0733e2d02c89b41e5ba25841f5804
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-15 22:27:05 +0300

    Add Exercise Solution

---
+++ Iterators and Comparators - Exercise/01. ListyIterator/ListyIterator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsandComparatorsExercise
+{
+    public class ListyIterator<T>
+    {
+        private List<T> elements;
+        private int index;
+        public ListyIterator(params T[] elements)
+        {
+            this.elements = new List<T>(elements);
+            index = 0;
+        }
+        public bool HasNext()
+        {
+            if (index < elements.Count-1)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool Move()
+        {
+            bool canMove = HasNext();
+            if (canMove)
+            {
+                ++index;
+            }
+            return canMove;
+        }
+        public void Print()
+        {
+            if (elements.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+                Console.WriteLine($"{elements[index]}");
+        }
+    }
+}
