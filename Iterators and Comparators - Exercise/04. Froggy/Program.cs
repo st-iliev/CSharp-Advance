6f748b68776565846f29514ef399d2fb95b986e8
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-15 22:30:04 +0300

    Add Exercise Solution

---
+++ Iterators and Comparators - Exercise/04. Froggy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lake lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(string.Join(", ",lake));
+
+        }
+    }
+}
