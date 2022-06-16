4586d2a40139a5c6e1f48ccb1951edc64d902d0d
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-16 22:19:41 +0300

    Add Exercise Solution

---
+++ Iterators and Comparators - Exercise/05. Comparing Objects/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Town { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = 0;
+            if (this.Name.CompareTo(other.Name)!=0)
+            {
+                result = this.Name.CompareTo(other.Name);
+            }
+            else if (this.Age.CompareTo(other.Age) != 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+            else if (this.Town.CompareTo(other.Town) != 0)
+            {
+                result = this.Town.CompareTo(other.Town);
+            }
+            return result;
+        }
+    }
+}
---
+++ Iterators and Comparators - Exercise/05. Comparing Objects/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> persons = new List<Person>();
+            string[] currentPerson = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (currentPerson[0]!="END")
+            {
+                string name = currentPerson[0];
+                int age = int.Parse(currentPerson[1]);
+                string town = currentPerson[2];
+                Person newPerson = new Person(name, age, town);
+                persons.Add(newPerson);
+                currentPerson = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+            int nPerson = int.Parse(Console.ReadLine());
+            Person comparePerson = persons[nPerson - 1];
+            int countMatches =-1;
+            int countNotEquals = 0;
+            foreach (var item in persons)
+            {
+                if (persons[nPerson - 1].CompareTo(item) == 0)
+                {
+                    countMatches++;
+                }
+                else
+                {
+                    countNotEquals++;
+                }
+            }
+            if (countMatches == 0)
+            {
+                Console.WriteLine($"No matches");
+            }
+            else
+            {
+            Console.WriteLine($"{countMatches+1} {countNotEquals} {persons.Count}");
+            }
+        }
+    }
+}
