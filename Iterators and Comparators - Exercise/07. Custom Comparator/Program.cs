4a522524f1bf64a56b7b04ad694fb59dc1ec1eaf
Stanislav Iliev <98229450+st-iliev@users.noreply.github.com>
2022-06-16 22:20:41 +0300

    Add Exercise Solution

---
+++ Iterators and Comparators - Exercise/07. Custom Comparator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomComparator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Func<int, int, int> sortedNumbers = (n1, n2) =>
+              {
+                  if (n1 % 2 == 0 && n2 % 2 != 0)
+                  {
+                      return -1;
+                  }
+                  else if (n1 % 2 != 0 && n2 % 2 == 0)
+                  {
+                      return 1;
+                  }
+                  else if ( n1 > n2)
+                  {
+                      return 1;
+                  }
+                  else if (n1 < n2)
+                  {
+                      return -1;
+                  }
+                  else
+                  {
+                      return 0;
+                  }
+              };
+            Array.Sort(numbers, (n1, n2) => sortedNumbers(n1, n2));
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
